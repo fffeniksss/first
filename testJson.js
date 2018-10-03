6833ffe8ae1eb4a0ae652222a455bdf5acab23fd
fffeniksss <39445294+fffeniksss@users.noreply.github.com>
2018-10-03 15:59:36 +0300

    Create testJson.js

---
+++ testJson.js
@@ -0,0 +1,26 @@
+[
+  {
+    "carName": "user1",
+    "km": 1
+  },
+  {
+    "carName": "user2",
+    "km": 2
+  },
+  {
+    "carName": "user3",
+    "km": 3
+  },
+  {
+    "carName": "user4",
+    "km": 4
+  },
+  {
+    "carName": "user5",
+    "km": 5
+  },
+  {
+    "carName": "user6",
+    "km": 6
+  }
+]
